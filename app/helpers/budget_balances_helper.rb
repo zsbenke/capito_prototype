82fbb724818b9fd572328a1937ef5dd8fa30d5a0
Zsolt Benke <zsolt@decoding.io>
2018-08-05 00:24:22 +0200

    Nicer number formatting.in budget inputs

--- app/helpers/budget_balances_helper.rb
+++ app/helpers/budget_balances_helper.rb
@@ -1,6 +1,11 @@
 module BudgetBalancesHelper
   def text_field_for_budget_balance_budgeted(budget_balance)
-    text_field_tag 'budget_balance[budgeted]', budget_balance.budgeted, autocomplete: :off,
+    budgeted = budget_balance.budgeted == 0.0 ? nil : budget_balance.budgeted
+    if budgeted.to_i.to_f == budgeted
+      budgeted = budgeted.to_i
+    end
+
+    text_field_tag 'budget_balance[budgeted]', budgeted, autocomplete: :off,
       data: {
         remote: true,
         url: budget_budget_balance_path(budget_id: 1, id: budget_balance.id, current_month: params[:current_month]),
